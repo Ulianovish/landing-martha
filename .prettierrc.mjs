e025b48312fa0798812bfa1e955ed1cbe9352bd7
Miguel Caro <ulianovish@gmail.com>
2024-04-20 15:27:26 -0500

    add configs and text

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,5 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: "none",
+  jsxBracketSameLine: false,
+  endOfLine: "auto",
   plugins: [
     "prettier-plugin-astro",
     "prettier-plugin-tailwindcss", // MUST come last
